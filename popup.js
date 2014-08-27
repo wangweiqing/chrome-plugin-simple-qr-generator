c6b3322f4b1f88518b6465ba6b14d37cb1a72dce
Weiqing Wang <wangwq.fnst@cn.fujitsu.com>
2014-08-27 15:02:10 +0800

    Change the QR creator to local.
    Add a jquery plugin qrcode to make sure QR will be created by local creator
    
    Signed-off-by: Weiqing Wang <wangwq.fnst@cn.fujitsu.com>

--- popup.js
+++ popup.js
@@ -6,17 +6,11 @@
 
 Zepto(function($) {
     chrome.tabs.getSelected(null, function(tab) {
-        var url = "https://chart.googleapis.com/chart?cht=qr"+
-                  "&chs=200x200&choe=UTF-8&chld=Q|0&chl=" + tab.url + "";
-
         if (tab.favIconUrl) {
             $("#fav").attr("src", tab.favIconUrl).show();
         }
-
-        $("#fav, #qr").bind("error", function(e) {
-            $(e.target).hide();
+        jQuery('#content').qrcode({
+            text    : tab.url
         });
-
-        $("#qr").attr("src", url);
     });
 });
